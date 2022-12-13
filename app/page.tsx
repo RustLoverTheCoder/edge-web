804c450b28e07a283a583ab6b8061aeeabf4b420
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2022-12-13 14:58:40 +0800

    feat: add ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Header, Hero, Footer, PrimaryFeatures } from './ui'
+import { Header, Hero, Footer, PrimaryFeatures } from 'components/home/ui'
 
 export default function Home() {
   return (
