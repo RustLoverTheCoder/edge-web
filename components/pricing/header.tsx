3facf3da916ec07420f095e60abc44dff707a570
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2022-12-14 09:34:36 +0800

    feat: add header color

--- components/pricing/header.tsx
+++ components/pricing/header.tsx
@@ -34,7 +34,7 @@ const Header = () => {
             </div>
           </div>
           <div className="hidden md:flex md:items-center md:space-x-6">
-            <Link href="#" className="text-base font-medium text-white hover:text-gray-300">
+            <Link href="#" className="text-base font-medium text-gray-500 hover:text-gray-300">
               Log in
             </Link>
             <Link href="#" className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-base font-medium text-white hover:bg-gray-700">
