92bd31b9a993008b45d44a2e7d9f3f674c0b7cc8
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2022-12-14 10:59:40 +0800

    feat: add inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 import './globals.css'
+import Script from 'next/script';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head />
+      <Script src='https://rsms.me/inter/inter.css' />
       <body>{children}</body>
     </html>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
