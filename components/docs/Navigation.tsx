379df915a402a8c809e18d4c391764994a269cef
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2022-12-15 15:44:16 +0800

    feat: add RootLayout

--- components/docs/Navigation.tsx
+++ components/docs/Navigation.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import clsx from 'clsx'
 import { Navigation } from './type'
 import { memo } from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navigation = ({ navigation, className }: { navigation: Array<Navigation>; className?: string }) => {
-  let router = useRouter()
+  const pathname = usePathname()
   return (
     <nav className={clsx('text-base lg:text-sm', className)}>
       <ul className="space-y-9">
@@ -16,18 +16,18 @@ const Navigation = ({ navigation, className }: { navigation: Array<Navigation>; 
               {section.links.map((link) => (
                 <li key={link.href} className="relative">
                   <Link href={link.href}>
-                    <a
+                    <div
                       className={clsx(
                         'block w-full pl-3.5 before:pointer-events-none before:absolute before:-left-1 before:top-1/2 before:h-1.5 before:w-1.5 before:-translate-y-1/2 before:rounded-full',
                         {
-                          'font-semibold text-sky-500 before:bg-sky-500': link.href === router.pathname,
+                          'font-semibold text-sky-500 before:bg-sky-500': link.href === pathname,
                           'text-slate-500 before:hidden before:bg-slate-300 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300':
-                            link.href !== router.pathname,
+                            link.href !== pathname,
                         }
                       )}
                     >
                       {link.title}
-                    </a>
+                    </div>
                   </Link>
                 </li>
               ))}
